554474ae5ce0f7edd8eda077184dcf4f1e05aa66
Nathan Sodja <sodjanathan@gmail.com>
2022-04-07 22:33:54 +0000

    create async wt promise, call promise func better than callback hell

--- 01-node-tutorial/app.js
+++ 01-node-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Welcome to Node Tutorial')
+const { readFile } = require('fs');
+
+// async code that returns a promise
+const getText = (path) => {
+	return new Promise((resolve, reject) => {
+		readFile(path, 'utf8', (err, data) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(data);
+			}
+		})
+	})
+}
+
+// calling the async function and chaining the promise
+const result = getText('./content/first.txt').then(result =>{console.log(result)}).catch(err =>{console.log(err)})
