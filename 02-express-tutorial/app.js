4ddd2d3d1b3db9b9862d7426ca478983cdbce8f9
Nathan Sodja <sodjanathan@gmail.com>
2022-04-24 14:04:50 +0000

    simple server with express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,26 +1,28 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const app = express();
 
-const homepage = readFileSync("./index.html", "utf8");
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-
-  if (url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(homepage);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/plain" });
-    res.write("about page");
-    res.end();
-  } else {
-    res.writeHead(404, { "content-type": "text/plain" });
-    res.write("page not found");
-    res.end();
-  }
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home Page");
 });
 
-server.listen(5000);
+app.get("/about", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("About Page");
+});
 
+app.all("*", (req, res) => {
+  res.status("404").send("<h1>Resource not found</h1>");
+});
 
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+// app.get
+// app.post
+// app.put
+// app.delete
+// app.all
+// app.use
+// app.listen
