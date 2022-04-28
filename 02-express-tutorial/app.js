42b910be90b3a88deb4c2c2c8e4954a25dad2e12
Nathan Sodja <sodjanathan@gmail.com>
2022-04-28 01:34:10 +0000

    query params basics

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -7,12 +7,19 @@ app.get("/", (req, res) => {
   res.send('<h1>Home Page</h1><a href="api/products">get products</a>');
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.status(200).json(newProducts);
+app.get("/api/products/:productID", (req, res) => {
+  console.log(req.params);
+  const { productID } = req.params;
+
+  const singleProduct = products.find(product => product.id === parseInt(productID));
+  console.log(singleProduct);
+  
+  if (!singleProduct) {
+    res.status(404).send('<h1>Product not found</h1><a href="/">Go back</a>');
+  } 
+    res.status(200).json(singleProduct);
+  
+
 });
 
 app.listen(5000, () => {
