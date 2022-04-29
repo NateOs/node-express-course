6314454ff7207e4380ea9459b0471836d1a0807c
Sodja Nathan <sodjanathan@gmail.com>
2022-04-29 16:44:05 +0000

    implementing meddleware

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,42 +1,22 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-  // res.status(200).json(products);
-  res.send('<h1>Home Page</h1><a href="api/products">get products</a>');
+// the logger middleware func
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  const ip = req.ip;
+
+  console.log(method, url, time, ip);
+  next(); // this passes on to the GET method, very important!
+  // either you send a response or you pass to next middleware
+};
+
+// the middleware implemented
+app.get("/", logger, function (req, res) {
+  res.send("Home");
 });
-
-app.get("/api/products/:productID", (req, res) => {
-  console.log(req.params);
-  const { productID } = req.params;
-
-  const singleProduct = products.find(product => product.id === parseInt(productID));
-  console.log(singleProduct);
-  
-  if (!singleProduct) {
-    res.status(404).send('<h1>Product not found</h1><a href="/">Go back</a>');
-  } 
-    res.status(200).json(singleProduct);
-});
-
-app.get('/api/v1/query/', (req, res) => {
-const {search, limit} = req.query;
-  let sortedProducts = [...products]
-
-  if (search) {
-    sortedProducts = sortedProducts.filter(product => product.name.startsWith(search))
-   return  res.status(200).json(sortedProducts)
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, parseInt(limit))
-   return  res.status(200).send(sortedProducts)
-  }
-  if (sortedProducts.length < 1) {
-   return  res.status(200).json({ success:true, data:[]})
-  }
-  res.status(200).json(sortedProducts)
-})
 
 app.listen(5000, () => {
   "Server running at port 5000";
