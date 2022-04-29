c0de693d4fdfceef92526a3cfe234270c00ffff6
Nathan Sodja <sodjanathan@gmail.com>
2022-04-29 01:53:50 +0000

    exploring query strings

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -18,9 +18,25 @@ app.get("/api/products/:productID", (req, res) => {
     res.status(404).send('<h1>Product not found</h1><a href="/">Go back</a>');
   } 
     res.status(200).json(singleProduct);
-  
-
 });
+
+app.get('/api/v1/query/', (req, res) => {
+const {search, limit} = req.query;
+  let sortedProducts = [...products]
+
+  if (search) {
+    sortedProducts = sortedProducts.filter(product => product.name.startsWith(search))
+   return  res.status(200).json(sortedProducts)
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, parseInt(limit))
+   return  res.status(200).send(sortedProducts)
+  }
+  if (sortedProducts.length < 1) {
+   return  res.status(200).json({ success:true, data:[]})
+  }
+  res.status(200).json(sortedProducts)
+})
 
 app.listen(5000, () => {
   "Server running at port 5000";
